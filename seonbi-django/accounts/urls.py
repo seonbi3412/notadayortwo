046112aa3c96db597fd2a476345d0f65f328a26f
EUNBI203 <unbi203@gmail.com>
2019-11-21 17:33:26 +0900

    accounts create

--- seonbi-django/accounts/urls.py
+++ seonbi-django/accounts/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
 
+app_name = 'accounts'
 urlpatterns = [
-    
+    path('', views.index, name='account_index'),
+    path('<int:user_pk>/', views.user_detail, name='account_user_detail'),
+    path('signup/', views.signup, name='account_signup'),
+    path('login/', views.login, name='account_login'),
+    path('logout/', views.logout, name='account_logout'),
+    path('<int:user_pk>/follow/', views.follow, name='follow'),
 ]
