0184cb86608b4861d36245f677913380cb67a83e
milknsoda <milknsoda9327@gmail.com>
2019-11-21 17:33:08 +0900

    Review model 수정

--- seonbi-django/movies/models.py
+++ seonbi-django/movies/models.py
@@ -22,4 +22,5 @@ class Review(models.Model):
     content = models.CharField(max_length=150)
     score = models.IntegerField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
 
