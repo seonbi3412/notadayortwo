66749ef856640db8683e38c73623fc7a09bd8e3e
dodonmountain <lkh151515@gmail.com>
2019-11-21 17:33:08 +0900

    Review model 수정

--- seonbi-django/movies/views.py
+++ seonbi-django/movies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Movie, Review
 
 # Create your views here.
@@ -8,3 +8,10 @@ def index(request):
         'movies': movies
     }
     return render(request, 'movies/index.html', context)
+
+def detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    context = {
+        'movie': movie
+    }
+    return render(request, 'movies/detail.html', context)
