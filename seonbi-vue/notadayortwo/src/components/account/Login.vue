61349ab1ed8b8abe1738e1a3c51fc0a4e7083c0a
EUNBI203 <unbi203@gmail.com>
2019-11-27 19:43:38 +0900

    update login_form & signup_form

--- seonbi-vue/notadayortwo/src/components/account/Login.vue
+++ seonbi-vue/notadayortwo/src/components/account/Login.vue
@@ -1,24 +1,38 @@
 <template>
-  <div class="mx-auto" style="width: 400px;">
-    <b-form @submit.prevent="login">
-			<b-form-group id="input-group-1" label="user name :" label-for="input-1">
-        <b-form-input
-          id="input-1"
-          v-model="credentials.username"
-          required
-          placeholder="Enter name"/>
-      </b-form-group>
-
-      <b-form-group id="input-group-2" label="Password :" label-for="input-2">
-        <b-form-input
-          id="input-2"
-					type="password"
-          v-model="credentials.password"
-          required />
-      </b-form-group>
-			<b-button type="submit">로그인</b-button>
-    </b-form>
-	</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-5 mx-auto">
+      <div id="first">
+        <div class="myform form ">
+          <div class="logo mb-3">
+            <div class="col-md-12 text-center">
+            <h1>Login</h1>
+            </div>
+          </div>
+          <form @submit.prevent="login" method="post" name="login">
+            <div class="form-group">
+              <label for="exampleInputEmail1">Username</label>
+              <input type="username" name="username"  class="form-control" id="username" placeholder="Enter Username" v-model="credentials.username" required>
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Password</label>
+              <input type="password" name="password" id="password"  class="form-control" placeholder="Enter Password" v-model="credentials.password" required>
+            </div>
+            <div class="form-group">
+              <p class="text-center">By bye</p>
+            </div>
+            <div class="col-md-12 text-center ">
+              <button type="submit" class=" btn btn-block mybtn btn-primary tx-tfm">Login</button>
+            </div>
+            <div class="form-group">
+              <p class="text-center">아직? <router-link to="/account/signup" id="signup">친구하기!</router-link></p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
